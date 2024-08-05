2e93f7cd10718049b7e96bcc874662b7147f4d4a
redo <142740981+redo-quest@users.noreply.github.com>
2024-08-05 17:11:12 +0200

    version, init

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const purgeServers = async () => {
 const main = async () => {
   while (true) {
     console.log(chalk.blue('[WORKER] Initialising...'));
-    console.log(chalk.blue('[WORKER] Made with ❤ by opencodebase.co.'));
+    console.log(chalk.blue('[WORKER] Pterodactyl Mass Purger v1. Made with ❤ by opencodebase.co.'));
     try {
       await purgeServers();
       console.log(chalk.green('[WORKER] Completed successfully.'));
